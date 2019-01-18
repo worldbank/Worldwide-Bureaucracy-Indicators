703ad6f0a5a46f7da1d1083aece67768b7f1aa7d
Luiza <luiza.c.andrade@gmail.com>
2019-01-18 18:57:49 -0500

    Dashboard: changed user interface layout

--- Dashboard/ui.R
+++ Dashboard/ui.R
@@ -1,24 +1,36 @@
-
-library(markdown)
+require(shinydashboard)
+require(shiny)
 
 wwbi <- read.csv("data.csv", header = T,stringsAsFactors = F)
 
-ui <- fluidPage(
-  
-  # App title ----
-  titlePanel("Worldwide Bureaucracy Indicators"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-      
-      selectInput("selected_country", "Select country:",
-                  choices = c(as.character(wwbi$countryname)))
 
-    ),
-    
-    downloadButton("report", "Generate report")
+ui <- navbarPage("Worldwide Bureaucracy Indicators",
+                 id = "nav",
+                 
+# Community level #######################################################################################
+  
+  tabPanel("Report",
+      dashboardPage(
+        
+        header <- dashboardHeader(disable = T
+        ),
+        
+        # Controls =================================================================================
+        
+        dashboardSidebar(
+          sidebarMenu(
+            
+            
+            selectInput("selected_country", "Select country:",
+                        choices = c(as.character(wwbi$countryname)))
+            
+          )
+        ),
+        
+        # Results ==========================================================================================
+        body <- dashboardBody(
+          downloadButton("report", "Generate report")
+        )
+    )
   )
 )
