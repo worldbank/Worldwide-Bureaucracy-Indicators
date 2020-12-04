06fb60595f3e50eb4aa0abf77c6382445e12bbda
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2020-12-04 16:16:06 -0500

    add fill and legend example

--- jsVisualizations/scripts/shiny/coffee/wwbi-coffee/coffee-tidy.R
+++ jsVisualizations/scripts/shiny/coffee/wwbi-coffee/coffee-tidy.R
@@ -4,6 +4,11 @@
 library(sf)
 library(rjson)
 library(leaflet)
+library(RColorBrewer)
+
+
+                  # Import World Bank Admin0 Files ----
+
 
 # import world json ----
 #world <- rjson::fromJSON(file = worldjson)
@@ -15,20 +20,72 @@ worldVars <- c("FID",       "OBJECTID",  "featurecla","LEVEL",     "TYPE",      
               "REGION_UN", "SUBREGION", "REGION_WB", "NAME_EN",  
               "WB_NAME",   "WB_RULES",  "WB_REGION", "Shape_Leng","Shape_Area","geometry")
 
+
+
+## make map dataframes
 world_sf_raw <- st_read(worldjson)
 
-world_sf     <-
+world_sf     <-   # working version 
   world_sf_raw %>%
   select(worldVars)  
+
+world_geo    <- world_sf %>%
+  select("ISO_A3", "geometry")
+
+
+
+
+
+
+                    # Merge with WWBI ----
+# import wwbi files from earlier code 
+load(file = file.path(wwbi_dat, "wwbi-tbls.Rdata"))
+
+
+# join using iso code 
+wwbi_geo <-
+  left_join(wwbi,
+            world_geo,
+            by = c('ctycode' = 'ISO_A3')) %>%
+  st_as_sf()
+
+
+
+
 
 
 # inside server ----
 
-m <- leaflet(data = world_sf) %>%
-  addTiles() %>%
-  addPolygons()
+## define color pallette 
+pal <- colorBin("YlOrRd", domain = wwbi_geo$ln_gdp, bins = 5)
 
-m
+## build map base
+m <- wwbi_geo %>%
+  filter(year == 2017) %>%
+  leaflet() %>%
+  addTiles() 
+
+## add fill
+m <- m %>%
+  addPolygons(
+    fillColor = ~pal(ln_gdp),
+    weight = 2,
+    opacity = 1,
+    color =  "white",
+    dashArray = 1,
+    fillOpacity = 0.8
+  )
+
+## add legend 
+m %>%
+  addLegend(
+    pal = pal,
+    position = "bottomright",
+    na.label = "No Info",
+    values = ~ln_gdp,
+    title = "Log GDP Per Capita"
+    
+  )
 
 
 
